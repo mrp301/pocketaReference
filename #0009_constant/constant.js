fb4c13b5c52fe9fbff71f3454e42d3e392e08506
H-1454 <h-1454@H-1454noMacBook-Pro.local>
2017-10-02 10:06:30 +0900

    cssファイル統一、スタイル修正

--- #0009_constant/constant.js
+++ #0009_constant/constant.js
@@ -1,9 +1,9 @@
 //#0010_例外処理
 
-try{
-  WINDOWS("VISRA?");
-}catch(e){ //eにはエラー内容が格納される。
-  alert("エラーは" +e+ "です");
-}finally{
-  alert("ミスですね。どうしましょう");
-}
+const a=1;
+var b=2;
+var c=3;
+var d=b+c;
+//a=b+c 定数の値は固定なのでエラーになる
+document.write("a=" +a+ "<br>");
+document.write("d=" +d);

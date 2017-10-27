e3aebf48edd86454474ad1a683f908bccad1ffe6
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-10-27 23:34:11 +0900

    #0038追加

---
+++ #0038_文字列を数値に変換/index.js
@@ -0,0 +1,11 @@
+//#0038
+
+function convFloat(){
+  var str = document.myFORM.myValue.value;
+  alert(parseFloat(str));
+}
+
+function convInt(){
+  var str = document.myFORM.myValue.value;
+  alert(parseInt(str));
+}

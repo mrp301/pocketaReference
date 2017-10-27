c8ddf14683dfce23eea0a6beeb6977e25bfdc9b8
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-10-27 23:06:22 +0900

    #0035完成

--- #0035_数値変換/index.js
+++ #0035_数値変換/index.js
@@ -2,10 +2,10 @@
 
 function setEval(){
   var str = document.myFORM.myValue.value;
-  document.getELementById("result").innerHTML = eval(str);
+  document.getElementById("result").innerHTML = eval(str); //数式から計算結果の数値に変換
 }
 
 function setToString(n){
   var str = eval(document.myFORM.myValue.value);
-  document.getELementById("result").innerHTML = toString(n);
+  document.getElementById("result").innerHTML = str.toString(n); //n進数に変換
 }

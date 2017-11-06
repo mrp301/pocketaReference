909e785819d870dd7ec60c948a7d8319286f88a7
H-1454 <h-1454@H-1454noMacBook-Pro.local>
2017-11-06 23:02:35 +0900

    0048追加

---
+++ #0048_配列要素検索/index.js
@@ -0,0 +1,8 @@
+//#0048
+
+var ele = document.getElementById("result");
+var fe = ["2011", "By", "kazuhiro", "Furuhata", "By", "KF"];
+ele.innerHTML = "indexOf('By') =" +fe.indexOf("By");
+ele.innerHTML += "<br>indexOf('Bye')=" +fe.indexOf("Bye");
+ele.innerHTML += "<br>lastIndexOf('By',4)=" +fe.lastIndexOf("By",4); //開始位置を4番目の要素から行う
+ele.innerHTML += "<br>lastIndexOf('Bye')=" +fe.lastIndexOf("Bye"); //開始位置を4番目の要素から行う

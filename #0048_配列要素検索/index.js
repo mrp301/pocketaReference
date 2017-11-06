2e87254fd35fdc29b731e9d940b580694074495b
hishinuma616 <hishinuma.yuta@a-tm.co.jp>
2017-11-06 23:13:33 +0900

    a

--- #0048_配列要素検索/index.js
+++ #0048_配列要素検索/index.js
@@ -5,4 +5,4 @@ var fe = ["2011", "By", "kazuhiro", "Furuhata", "By", "KF"];
 ele.innerHTML = "indexOf('By') =" +fe.indexOf("By");
 ele.innerHTML += "<br>indexOf('Bye')=" +fe.indexOf("Bye");
 ele.innerHTML += "<br>lastIndexOf('By',4)=" +fe.lastIndexOf("By",4); //開始位置を4番目の要素から行う
-ele.innerHTML += "<br>lastIndexOf('Bye')=" +fe.lastIndexOf("Bye"); //開始位置を4番目の要素から行う
+ele.innerHTML += "<br>lastIndexOf('Bye')=" +fe.lastIndexOf("Bye"); //開始位置を4番目の要素から行うsa

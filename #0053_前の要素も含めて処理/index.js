7658b770f9b5a1cee5181e3794c37b7e09ebdf02
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-10 14:10:52 +0900

    53修正

--- #0053_前の要素も含めて処理/index.js
+++ #0053_前の要素も含めて処理/index.js
@@ -1,9 +1,12 @@
 //#0053
 
-function connection(prev,value,index,array){
+function connection(prev,value,index,array){ //前の要素、要素、位置、配列　がパラメータとして渡される。
   return prev + value;
 }
-var data = ["A","B","C","D","E"];
+var data = [1,2,3,4,5];
+data.forEach(function(value,index,array){ //要素、位置、配列
+  var e = document.getElementById("result").innerHTML += index+ "番目：" +value+ "<br>"; 
+})
 var d1 = data.reduce(connection);//前方から処理
 var d2 = data.reduceRight(connection,"■"); //後方から処理
-document.getElementById("result").innerHTML = d1 +"<br>"+ d2;
+document.getElementById("result").innerHTML += d1 +"<br>"+ d2;

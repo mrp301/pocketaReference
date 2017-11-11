391ec524560db57cbf022b8ec078a1494a373a2d
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-12 00:36:58 +0900

    62追加

--- #0062_文字列の抜き出し/index.js
+++ #0062_文字列の抜き出し/index.js
@@ -4,10 +4,19 @@ var ele = document.getElementById("result");
 var str = "K.FuRuhata";
 var str2 = "Presented/By/KaZuhiro/FuRuhata";
 for(var i=0; i<str.length; i++) ele.innerHTML += str.charAt(i) + "<br>"; //文字列strのi番目の文字を抜き出す。
-ele.innrtHTML += "<hr>";
+ele.innerHTML += "<hr>";
 
 for(i=0; i<str.length; i++) ele.innerHTML += str.substr(0,i) + "<br>"; //0番目からi文字目まで抜き出す
-ele.innrtHTML += "<hr>";
+ele.innerHTML += "<hr>";
 
-for(i=0; i<str.length; i++) ele.innertHTML += str.substring(i,i+3) + "<br>"; //0番目からi+3番目まで抜き出す
-ele.innrtHTML += "<hr>";
+for(i=0; i<str.length; i++) ele.innerHTML += str.substring(i,i+3) + "<br>"; //0番目からi+3番目まで抜き出す
+ele.innerHTML += "<hr>";
+
+for(i=0 ;i<str.length; i++) ele.innerHTML += str.slice(i,99) + "<br>"; //
+ele.innerHTML += "<hr>";
+
+for(i=0 ;i<str.length; i++) ele.innerHTML += str.slice(0,-i) + "<br>"; //
+ele.innerHTML += "<hr>";
+
+var conv = str2.split("/"); //指定文字で分割
+for(i=0; i<conv.length; i++) ele.innerHTML += conv[i] + "<br>"; //分割した文字は配列に格納される

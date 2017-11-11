1e857c854398c789237ec1ecea26f9c2d72f9b1f
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-11 13:26:16 +0900

    58保存し忘れ

--- #0058_コード:文字変換/index.js
+++ #0058_コード:文字変換/index.js
@@ -2,4 +2,4 @@
 
 var ele = document.getElementById("result");
 ele.innerHTML = "ABC".charCodeAt(0)+ "<br>"; //文字列ABCの0番目の文字AをISO Latin-1コードで表示する。
-ele.innerHTML += String.fromCharCode(0x3042, 0x3044);
+ele.innerHTML += String.fromCharCode(0x3042, 0x3044); //ISO Latin-1コードを文字列として表示する。

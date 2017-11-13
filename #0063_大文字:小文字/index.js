aef829da3c8c48816514dc9ad7cb8984a7e4d809
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-13 22:48:49 +0900

    63追加

---
+++ #0063_大文字:小文字/index.js
@@ -0,0 +1,8 @@
+//#0063
+
+var ele = document.getElementById("result");
+str = "K.FuRuhata";
+ele.innerHTML = str.toLowerCase() +"<br>";
+ele.innerHTML += str.toUpperCase() +"<br>";
+ele.innerHTML += str.toLocaleLowerCase() +"<br>";
+ele.innerHTML += str.toLocaleUpperCase() +"<br>";

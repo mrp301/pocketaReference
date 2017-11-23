f3990e452ed9809e942e8a12936910f98916ac2a
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-23 12:49:02 +0900

    87追加

---
+++ #0087_表示可能範囲/index.js
@@ -0,0 +1,7 @@
+//#0087
+
+var txt = "有効な横幅 = " +screen.availWidth+ "pixcel<br>";
+txt += "有効な縦幅 = " +screen.availHeight+ "pixcel<br>";
+txt += "有効なX座標 = " +screen.availLeft+ "pixcel<br>";
+txt += "有効なY座標 = " +screen.availRight+ "pixcel<br>";
+document.getElementById("result").innerHTML = txt;

b3e2519733abf8f02a4b6455a95e1398f20d522e
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-24 23:31:10 +0900

    91追加

---
+++ #0091_ウインドウの縦幅:横幅/index.js
@@ -0,0 +1,8 @@
+//#0091
+
+var ele = document.getElementById("result");
+ele.innerHTML = "innerWidth = " +window.innerWidth+ "<br>";
+ele.innerHTML += "innerHeight = " +window.innerHeight+ "<br>";
+ele.innerHTML += "outerWidth = " +window.outerWidth+ "<br>";
+ele.innerHTML += "outerHeight = " +window.outerHeight+ "<br>";
+

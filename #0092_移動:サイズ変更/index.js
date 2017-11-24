2e2a243334341ae5751e24daed18c8c711baf6bb
hishinuma616 <dnbwoodleaves3616@gmail.com>
2017-11-24 23:52:17 +0900

    92

---
+++ #0092_移動:サイズ変更/index.js
@@ -0,0 +1,17 @@
+//#0092
+
+function mvBY(x,y){
+  moveBy(x.value, y.value);
+}
+function mvTO(x, y){
+  moveTo(x.value, y.value);
+}
+
+function rsBY(x, y){
+  resizeBy(x.value, y.value);
+}
+
+function rsTO(x, y){
+  resizeTo(x.value, y.value);
+}
+

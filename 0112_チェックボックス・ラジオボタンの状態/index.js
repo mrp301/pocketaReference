a564e478eea4a5c2f32384107a444676cecdce8d
hishinuma616 <dnbwoodleaves3616@gmail.com>
2018-01-25 02:12:25 +0900

    113

---
+++ 0112_チェックボックス・ラジオボタンの状態/index.js
@@ -0,0 +1,3 @@
+function check(fObj){
+  alert(fObj.checked);
+}
---
+++ 0113_名前・ID/index.js
@@ -0,0 +1,5 @@
+var fObj = document.getElementById("tFld");
+var txt = "className = "+ fObj.className +"<br>";
+txt += "ID =" +fObj.id+ "<br>";
+txt += "UniqueID =" +fObj.uniqueID;
+document.getElementById("result").innerHTML = txt;
